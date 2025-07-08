9b6f5a5e47fa4f719830918b6f6f63b8b0a8e2e7
Rakshit Negi <122672621+rakshitnegi1234@users.noreply.github.com>
2025-07-09 02:32:53 +0530

    Update auth.controller.js

--- Backend/Controller/auth.controller.js
+++ Backend/Controller/auth.controller.js
@@ -31,8 +31,8 @@ export const signup = async (req,res) =>
         res.cookie("token", token, {
           httpOnly: true,
           maxAge: 7 * 24 * 60 * 60 * 1000, 
-          sameSite: "strict",
-          secure: false 
+          sameSite: "none",
+          secure: true 
         });   // token naam se haii aur uske rules haii
         
 
@@ -74,8 +74,8 @@ export const login = async (req,res) =>
           res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000, 
-            sameSite: "strict",
-            secure: false 
+            sameSite: "none",
+            secure: true 
           });
           
   
