be64b8b92969fe464686f475669a227ccf67c64f
Rakshit Negi <122672621+rakshitnegi1234@users.noreply.github.com>
2025-07-09 02:41:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: "https://a-i-assistant-1232.onrender.com",
+        origin: "https://a-i-assistant-3000.onrender.com",
         credentials:true
     }
 ));
